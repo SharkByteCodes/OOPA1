3696f2edd4af920c33e6cda14629e28e7ba93137
SharkByteCodes <124321575+SharkByteCodes@users.noreply.github.com>
2025-05-18 02:04:23 -0400

    Task 3 completed. Uses same math as provided example.

--- GME1003GoblinDanceParty/Game1.cs
+++ GME1003GoblinDanceParty/Game1.cs
@@ -19,6 +19,7 @@ namespace GME1003GoblinDanceParty
         private List<int> _starsY;      //list of star y-coordinates
 
         private List<float> _starRotate; // Task 2 (declare)
+        private List<float> _starOpacity; // Task 3 (delcare)
 
         private Texture2D _starSprite;  //the sprite image for our star
 
@@ -50,8 +51,9 @@ namespace GME1003GoblinDanceParty
             _starsX = new List<int>();  //stars X coordinate
             _starsY = new List<int>();  //stars Y coordinate
             
+            //insert assigned list initializations
             _starRotate = new List<float>(); // Task 2 (initialize list)
-            
+            _starOpacity = new List<float>(); // Task 3 (initialize list)
             
             _starColor = new Color(128 + _rng.Next(0,129), 128 + _rng.Next(0, 129), 128 + _rng.Next(0, 129));                   //this is a "relatively" easy way to create random colors
             _starScale = _rng.Next(50, 100) / 200f; //this will affect the size of the stars
@@ -73,21 +75,35 @@ namespace GME1003GoblinDanceParty
                 _starsY.Add(_rng.Next(0, 481)); //all star y-coordinates are between 0 and 480
             }
 
+            // List of star rotation loop
             for (int i = 0; i < _numStars; i++) // Task 2 (loop count for each star)
             {
                 _starRotate.Add(_rng.NextSingle()); // Why does this work now?
             }
+            
+            // Console Debuggy information
+            Console.WriteLine("---------------");
+            Console.WriteLine("Star 55 has volunteered as tribute."); // or has it?
+            Console.WriteLine("X position: " + _starsX[55] + ", Y position: " + _starsY[55]);
             Console.WriteLine("Number of items in rotation list: " + _starRotate.Count);
             Console.WriteLine("Rotation of star number 55: " + _starRotate[55]); // Debug Star Rotation on star 55
+            
+            // List of star opacity (Task 3)
+            for (int i = 0; i < _numStars; i++)
+            {
+                _starOpacity.Add(_rng.Next(25,100) /100f); // tested with NextSingle, couldn't set MinValue to .25f
+            }
+            Console.WriteLine("Opacity of star number 55: " + _starOpacity[55]);
+            
             //TODO: 6. Custom Background (800x480), maybe something custom , some other cool stuff to add
             
             //ToDo: 5. List of Colors
             
             //ToDo: 4. List of scale values
 
-            //ToDo: 3. List of transparency values
+            //ToDo: 3. List of transparency values (Complete)
 
-            //ToDo: 2. List of rotation values (Complete, sloppy)
+            //ToDo: 2. List of rotation values (Complete, sloppy?)
 
 
             base.Initialize();
@@ -138,8 +154,8 @@ namespace GME1003GoblinDanceParty
                 _spriteBatch.Draw(_starSprite, 
                     new Vector2(_starsX[i], _starsY[i]),    //set the star position
                     null,                                   //ignore this
-                    _starColor * _starTransparency,         //set colour and transparency
-                    _starRotate[i],                          //set rotation (now uses rotation)
+                    _starColor * _starOpacity[i],         //set colour and transparency (now uses per star list)
+                    _starRotate[i],                          //set rotation (now uses rotation list)
                     new Vector2(_starSprite.Width / 2, _starSprite.Height / 2), //ignore this
                     new Vector2(_starScale, _starScale),    //set scale (same number 2x)
                     SpriteEffects.None,                     //ignore this
--- README.md
+++ README.md
@@ -14,14 +14,14 @@ This README helps discuss, in brief, the work completed over the course of this 
 ### Tasks for MonoGame Refresh (6 total)
 1. - [x] Each star == random number of stars (~50-300).
 2. - [x] Each star == different rotation. [^1]
-3. - [ ] Each star == different transparency.
+3. - [x] Each star == different transparency.
 4. - [ ] Each star == different size/scale.
 5. - [ ] Each star == different colour.
 6. - [ ] Customize and personalize (custom draw statement for new background, changing stars to something cool).
 
-Task completion: ```33.6/100%```
+Task completion: ```49.8/100%```
 
-[^1]: ~~Sloppy~~ Fixed! Uses NextSingle method now. 
+[^1]: ~~Sloppy~~ Fixed! Uses NextSingle method now. Short span between last and this commit (retraced steps with "Redo" button).
 
 ---
 
